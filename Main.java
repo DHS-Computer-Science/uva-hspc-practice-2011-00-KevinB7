736301ac554e43400c4e8b137c58bace3206b562
Kevin Bryant <kevinb7@users.noreply.github.com>
2016-03-02 08:17:53 -0500

    I did something

--- Main.java
+++ Main.java
@@ -2,9 +2,17 @@
 //TODO: <date>
 //Practice 2011 - 00: Hello Judge
 
-public class  {
+import java.util.*;
+public class Main {
   public static void main(String[] args) {
 
+   Scanner scan = new Scanner(System.in);
+   
+   int N = scan.nextInt();
+   
+   for(int i=1; i<=N; i++)
+      System.out.println("Hello World, Judge " + i);
+   
     System.exit(0);
   }
 }
